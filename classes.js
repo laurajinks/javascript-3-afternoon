cf438a3add09d3f9d7a4579a596fe3dbada79508
Laura <laura.jinks@gmail.com>
2018-11-28 20:47:05 -0600

    Solve the problems

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    let str = `${this.first_name} ${this.last_name} Widget`;
+    return str;
+}
+}
 
 
 
@@ -49,7 +60,22 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor (first_name, last_name, email, age) {
+    super (first_name, last_name, email, age); 
+    this.reports = [];
+  }
+
+
+    hire (employee) {
+      this.reports.push(employee);
+    }
+
+    fire (index) {
+          this.reports.splice(index, 1);
+          return this.reports;
+    }
+  }
 
 
 
@@ -75,7 +101,49 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor (first_name,last_name, email, age, reports){
+    super (first_name,last_name, email, age, reports);
+      this.title = 'Not a manager';
+      this.bonus = 0;
+      }
+
+      updateTitle () {
+        if (this.reports.length > 100) {
+          this.title = 'Bestest Manager';
+        }
+        else if (this.reports.length > 50) {
+          this.title = 'Manager Plus';
+        }
+        else if (this.reports.length > 10) {
+          this.title = 'Manager';
+        }
+        else if (this.reports.length > 3) {
+          this.title = 'Mostly Manager';
+        }
+        else if (this.reports.length > 0) {
+          this.title = 'Barely Manager';
+        }
+        else {
+          this.title = 'Not a manager';
+        }
+      }
+
+    updateBonus () {
+      this.bonus += 100;
+      }
+
+    hire (employee) {
+      this.reports.push(employee);
+      this.updateTitle();
+      }
+  
+    fire (index) {
+        this.reports.splice(index, 1);
+        this.updateBonus();
+          return this.reports;
+      }
+    }
 
 
 
@@ -102,6 +170,28 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor () {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
 
+  makeWidgets (num) { 
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += (num/50);
+  }
+
+  fixMachine () {
+    this.needs_reboot = true;
+  }
+
+  reboot () { 
+      return (() => {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    });
+  }
+    //return anon func when mcahine is done booting that decreases w&t by 10
+  }
 
